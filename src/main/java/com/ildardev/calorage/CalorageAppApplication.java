8f61265f3271eba573a4d401ef2622a4092a02da
isaifeev <ildar.saifeev@taskdata.ru>
2020-01-14 18:09:02 +0300

    Initial commit

---
+++ src/main/java/com/ildardev/calorage/CalorageAppApplication.java
@@ -0,0 +1,13 @@
+package com.ildardev.calorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ildardev/calorage/CalorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ildardev.calorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
