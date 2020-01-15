803ad5c1d546416b4fe23d957a019c5e0011b680
isaifeev <ildar.saifeev@taskdata.ru>
2020-01-15 20:22:41 +0300

    Disable csrf; configure ModelMapper for strict matching

--- src/main/java/com/ildardev/calorage/CalorageAppApplication.java
+++ src/main/java/com/ildardev/calorage/CalorageAppApplication.java
@@ -1,6 +1,7 @@
 package com.ildardev.calorage;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ public class CalorageAppApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		return modelMapper;
 	}
 }
